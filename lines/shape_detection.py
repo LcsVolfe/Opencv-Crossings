afffd3baa2f65bcd96427f4184cd61da15918d91
Lucas - Fedora <lcsvolfe@gmail.com>
2019-12-04 01:03:17 -0300

    contator

--- lines/shape_detection.py
+++ lines/shape_detection.py
@@ -8,8 +8,11 @@ cap = cv2.VideoCapture('v.mp4')
 
 
 contador = 0
-ultimoGridVisitado = 0
-
+ultimoGridVisitado = None
+#safe_view = 0
+grids_visitados=[]
+contador_frame = 0
+grid_detection = True
 
 def conferirIndicesArray(arr, novoValor):    
     if (arr[-1]+100) < novoValor:
@@ -58,9 +61,16 @@ def determinarGrids(frame, width, height):
     
 
 while(cap.isOpened()):
+    
+    # CONTADOR PARA FUNÇÃO DE DETECT
+    contador_frame = contador_frame+1
+    if contador_frame == 10:
+        grid_detection = True
+        contador_frame = 0
+    
     ret, frame = cap.read()
     height, width, aux = frame.shape
-    frame_drawable = frame    
+    frame_drawable = frame  
     
     # THRESHOULD PARA DETERMINAR GRIDS 
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -70,17 +80,11 @@ while(cap.isOpened()):
     red = cv2.bitwise_and(frame, frame, mask=red_mask)
     gray = cv2.cvtColor(red, cv2.COLOR_RGB2GRAY)
     gray_thresh = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)[1]
-
-    # cv2.imshow("Image", gray_thresh)
     
     x_up, x_down, y_up, y_down = determinarGrids(gray_thresh,width, height)
     
-    # print('x_up',x_up, '----')
-    # print('x_down',x_down, '----')
-    # print('x_up',y_up, '---')
-    # print('x_down',y_down, '\n')
-    
-    if len(x_up) > 1 and len(x_down) > 1 and len(y_up) > 1 and len(y_down) > 1:    
+    if grid_detection and len(x_up) > 1 and len(x_down) > 1 and len(y_up) > 1 and len(y_down) > 1:
+        grid_detection=False
         cv2.line(frame_drawable, (x_up[0], 0), (x_down[0], height-1), (255, 100, 0), 3)
         cv2.line(frame_drawable, (x_up[1], 0), (x_down[1], height-1), (255, 100, 0), 3)
         cv2.line(frame_drawable, (0, y_up[0]), (width-1, y_down[0]), (255, 100, 0), 3)
@@ -95,6 +99,8 @@ while(cap.isOpened()):
         cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
 
+
+        
         # LOOP NO CONTORNO PARA ENCONTRAR O CENTER_ID
         for c in cnts:
             # ENCONTRA O CENTRO DA IMAGEM
@@ -106,48 +112,51 @@ while(cap.isOpened()):
             else:
                 cX, cY = 0, 0    
             
+            
             # DESENHA ENCIMA DO CENTER_ID
             cv2.drawContours(frame_drawable, [c], -1, (0, 255, 0), 2)
             cv2.circle(thresh, (cX, cY), 7, (255, 255, 255), -1)
             cv2.putText(frame_drawable, "center", (cX - 20, cY - 20),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
         
-            
             if gray_thresh[cY, cX] == 0:
-                
-                print(ultimoGridVisitado, ' ultimoGridVisitado ')
-                print(cY > y_up[1], cY , y_up[1], ' cY > y_up[1] 3')
-                print(cY > y_up[0], cY , y_up[0], ' cY > y_up[0] 2')
-                print(cY < y_up[0], cY , y_up[0], ' cY < y_up[0] 1')
-                
-                # 3 linha
-                if cY > y_up[1] and ultimoGridVisitado != 3:
-                    # print(' 3 linha \n')
-                    ultimoGridVisitado=3
-                    contador = contador+1
-                
-                # 2 linha
-                elif cY > y_up[0] and cY > y_up[1] and ultimoGridVisitado != 2:
-                    ultimoGridVisitado=2
-                    # print(' 2 linha \n')
-                    contador = contador+1
+                # print(ultimoGridVisitado, ' ultimoGridVisitado ')
+                # print(cY > y_up[1], cY > y_down[1], cY , y_up[1], y_down[1], ' \n')
+                print(grids_visitados)           
                     
-                # 1 linha
-                elif cY < y_up[0] and ultimoGridVisitado != 1:
-                    # print('1 linha \n')        
-                    ultimoGridVisitado=1
+                
+                media_cX_0 = (x_up[0] + x_down[0])/2
+                media_cX_1 = (x_up[1] + x_down[1])/2
+                
+                if cX > media_cX_1 and ultimoGridVisitado != 3:
+                    ultimoGridVisitado=3
+                    grids_visitados.append(3)
+                        
                     contador = contador+1
                 else:
-                    print('else')
-                print(contador, 'count \n')
+                    ultimoGridVisitado=1  
+                # # # 2 linha
+                # elif ((cX > media_cX_0) or (cX > media_cX_1)) and ultimoGridVisitado != 2:
+                #     ultimoGridVisitado=2
+                #     grids_visitados.append(2)
+                #     contador = contador+1
+                    
+                # # # 1 linha
+                # elif cX < media_cX_0 and ultimoGridVisitado != 1:
+                #     ultimoGridVisitado=1
+                #     grids_visitados.append(1)
+                #     contador = contador+1
+    
                 
-                time.sleep(1)
+                time.sleep(0.5)
             
             cv2.putText(frame_drawable, ("Contador: " + str(contador)), (50, 50),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)   
             
+            cv2.putText(frame_drawable, ("Array: " + str(len(grids_visitados))), (350, 50),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)   
+            
             cv2.imshow("Image", frame_drawable)
-        
         
         # cv2.imshow('frame',thresh)
         if cv2.waitKey(1) & 0xFF == ord('q'):
